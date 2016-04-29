cebf49e41ee3ba1fdfdf6bbb857da5034b89f704
Dave For Projects <daveforprojects@gmail.com>
2016-04-29 19:17:09 -0400

    Create directory structure

---
+++ python/encryption/version01/encrypt_string.py
@@ -0,0 +1,40 @@
+import create_key as ck
+import string as s
+
+
+class Encryption: 
+    def __init__(self):
+        self.description = "Encryption method place holder"
+        
+        
+    def numeric_encode(self, plain_text):    
+        numeric_text = []
+        for p in plain_text:
+            if p in s.letters:
+                numeric_text.append(s.letters.index(p))
+            else:
+                #Will drop punctuation and preserve spaces
+                numeric_text.append(99)
+        return numeric_text
+
+        
+    def encrypt(self, plain_text, secret_key):
+        conv = ck.Converter()
+        conv.set_key(secret_key) 
+        num_text = self.numeric_encode(plain_text)
+        cypher_text = []
+        for n in num_text:
+            cypher_text.append(conv.get_key(n))
+        return(cypher_text)
+        
+        
+    def decrypt(self, cypher_text, secret_key):
+        conv = ck.Converter()
+        conv.set_key(secret_key) 
+        num_text = self.numeric_encode(cypher_text)
+        plain_text = []
+        for n in num_text:
+            plain_text.append(conv.get_key(n))
+        return plain_text
+    
+#EOF
