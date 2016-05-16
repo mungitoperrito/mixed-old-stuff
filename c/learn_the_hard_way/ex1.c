c1ef825d45affeb39334622cbc75d7479cc07908
Dave For Projects <daveforprojects@gmail.com>
2016-05-16 08:18:41 -0400

    Create C directory

---
+++ c/learn_the_hard_way/ex1.c
@@ -0,0 +1,12 @@
+//c.lerncodethehardway.org ex1
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    puts("Hola");             //Original string
+    puts("H\no\nl\na");       //Extra credit -- newline
+    puts("H\012o\012l\012a"); //Extra credit -- encoded newline
+
+    return 0;
+}
---
+++ c/learn_the_hard_way/ex3alt.c
@@ -0,0 +1,16 @@
+//c.lerncodethehardway.org ex2
+
+#include <stdio.h>
+#include <stdint.h>
+
+int8_t main(int8_t argc, int8_t *argv[])
+{
+    int8_t age = 10;
+    int8_t height = 72;
+
+    printf("I am %d years old.\n", age);
+    printf("I am %d inches tall.\n", height);
+
+    return 0;
+}
+
