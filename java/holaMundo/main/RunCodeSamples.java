4f24c7ddb435c5e59b8cdc6ce8db924f6f0f4a9c
Dave For Projects <daveforprojects@gmail.com>
2017-09-13 13:30:47 -0400

    Hello World with code runner

---
+++ java/holaMundo/main/RunCodeSamples.java
@@ -0,0 +1,17 @@
+/*
+ * Just a main class to call the exercise classes
+ */
+package main;
+
+/**
+ *
+ * @author dave
+ */
+import pets.Pet;
+        
+public class RunCodeSamples {
+    public static void main(String[] args){
+         HolaMungo.speak();   
+    }
+    
+}
