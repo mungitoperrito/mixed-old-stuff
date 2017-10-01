bfc1161bd18dbe0e1e6238a720e35e2d77253e33
Dave For Projects <daveforprojects@gmail.com>
2017-10-01 14:36:31 -0400

    Explain #define gotcha

---
+++ testcode/define_gotchas.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#define TWO 1 + 1
+#define WHAT TWO * (TWO + TWO)
+#define HMM 1 + 1 * (1 + 1 + 1 + 1)
+
+int main(int argc, char **argv)
+{
+
+    printf("T %d\n", TWO);
+    // Preprocessor substitutes then evaluates, so 5 instead of 8
+    printf("W %d\n", WHAT);
+    printf("H %d\n", HMM);
+
+	return 0;
+}
+
+/* 
+OUTPUT
+
+T 2
+W 5
+H 5
+
+*/
