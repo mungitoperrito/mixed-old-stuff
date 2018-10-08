ca9842d0fb57c75df3dd461f6e46b7ad659e786f
Dave For Projects <daveforprojects@gmail.com>
2018-10-07 22:18:43 -0400

    Get followers, print to csv

--- utilities/instagram-stats.py
+++ utilities/instagram-stats.py
@@ -7,9 +7,13 @@ Extract and copy the json files to a subdirectory called json-input
 Run this script from directly above the subdirectory
 '''
 import json
+import csv
+import sys
+import datetime as dt
+from collections import defaultdict
 
 
-def process_followers():
+def get_followers():
     try:
         with open("json-input/connections.json", "r") \
              as connections_file:
@@ -20,20 +24,36 @@ def process_followers():
         print("Other file error: {}".format(e))
     
     '''
-    Connection types 2018-10-07
+    Connection types as of 2018-10-07
         blocked_users
         follow_requests_sent
         followers   # accounts that follow you
         following   # accounts that you follow
         following_hashtags
     '''
-    print(len(connections['followers']))
-    for k,v in connections['followers'].items():
-        print("{} {}".format(k, v))
-
+    followers = {}
+    followers_by_date = defaultdict(list)
+    for follower,join_date in connections['followers'].items():
+        followers[follower] = join_date[:10]
+        jd_date = dt.datetime.date(dt.datetime.strptime(
+                              join_date, '%Y-%m-%dT%H:%M:%S'))
+        followers_by_date[jd_date].append(follower)
+        
+    try:
+        with open("followers.csv", "w") as output:
+            # For dev / debugging
+            # w = csv.writer(sys.stderr)
+            w = csv.writer(output)
+            w.writerows(followers.items())
+    except IOError as ioe:
+            print("IOError: {}".format(ioe))
+    except Exception as e:
+            print("SOme other error: {}".format(e))
+            
+    return followers_by_date
 
 def main():
-    process_followers()
+    followers = get_followers()
     
     
 if __name__ == "__main__":
