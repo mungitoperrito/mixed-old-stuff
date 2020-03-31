f868070864b2a12d986cbe403047ce201937cdef
Dave For Projects <daveforprojects@gmail.com>
2020-03-30 23:08:01 -0400

    Additional tests

--- utilities/test_find_pairs_pt.py
+++ utilities/test_find_pairs_pt.py
@@ -37,20 +37,45 @@ def test_use_both_end_values():
     assert fp.find_pairs_simple(test_array) == response
     assert fp.find_pairs(test_array) == response
 
-  
-'''
-    >>> find_pairs_simple([9,6,1,4,7])
-    9,1
-    6,4
-    >>> find_pairs_simple([5])
-    
-    >>> find_pairs_simple([5,5])
-    5,5
-    >>> find_pairs_simple([1,3,7,5,9])
-    1,9
-    3,7
-    >>> find_pairs_simple([1,3,7,5,9], 14)
-    5,9
-    >>> find_pairs_simple([13,-3,7,5,9])
-    13,-3
-'''    
+
+def test_avoid_repeated_single_value():
+    test_array = [5]
+    response = []
+    assert fp.find_pairs_simple(test_array) == response
+    assert fp.find_pairs(test_array) == response
+
+
+def test_repeated_values():
+    test_array = [5,5]
+    response = [(5,5)]
+    assert fp.find_pairs_simple(test_array) == response
+    assert fp.find_pairs(test_array) == response
+
+
+def test_multiple_pairs():
+    test_array = [9,6,1,4,7]
+    response = [(9,1), (6,4)]
+    assert fp.find_pairs_simple(test_array) == response
+    assert fp.find_pairs(test_array) == response
+
+
+def test_multiple_pairs_repeated_value():
+    test_array = [9,6,1,4,7,1]
+    response = [(9,1), (9,1), (6,4)]
+    assert fp.find_pairs_simple(test_array) == response
+    assert fp.find_pairs(test_array) == response
+
+
+def test_alternate_target():
+    test_array = [1,3,7,5,9]
+    target_value = 14
+    response = [(5,9)]
+    assert fp.find_pairs_simple(test_array, target_value) == response
+    assert fp.find_pairs(test_array, target_value) == response
+ 
+ 
+def test_negative_values():
+    test_array = [13,-3,7,5,9]
+    response = [(13,-3)]
+    assert fp.find_pairs_simple(test_array) == response
+    assert fp.find_pairs(test_array) == response 
