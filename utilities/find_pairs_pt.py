dbb8bad20ecdc1a62b6983d270465f0179ced386
Dave For Projects <daveforprojects@gmail.com>
2020-03-30 22:30:44 -0400

    Better return values for test functions

--- utilities/find_pairs_pt.py
+++ utilities/find_pairs_pt.py
@@ -4,21 +4,24 @@
 # NOTE: This version has been updated to python3 
 
 def find_pairs_simple(candidate_array, TARGET_VALUE=10):
+    pairs = []
+    
     for i in range(len(candidate_array)):
         for j in range(i + 1, len(candidate_array)):
             if (TARGET_VALUE == candidate_array[i] + candidate_array[j]):
                 if RUN_STANDALONE:
-                   print(f"{candidate_array[i]}, {candidate_array[j]}")
+                   pairs.append((candidate_array[i], candidate_array[j]))
                 else:
                    None
 
-    return 0
+    return pairs
 
 
 
 def find_pairs(candidate_array, TARGET_VALUE=10):
     from collections import defaultdict
     positions = defaultdict(list)
+    pairs = []
      
     #Read everything into a dictionary, storing the original array position 
     for i in range(len(candidate_array)):
@@ -31,26 +34,24 @@ def find_pairs(candidate_array, TARGET_VALUE=10):
             for p in positions[pair_value]:
                 if p > i:
                     if RUN_STANDALONE:
-                       print(f"{candidate_array[i]}, {pair_value}")
+                       pairs.append((candidate_array[i], pair_value))
                     else:
                        None
-    return 0
+    return pairs
       
     
 
 if "__main__" == __name__:
    RUN_STANDALONE = True
    
-   # A few demo tests for printing in case the file is run standalone. 
+   # A few demo tests for running standalone. 
    # Proper tests are in the pytest file
    
    smoke_tests = [([9,1,6], "9,1"), ([1,3,7,5,9], "1,9  3,7"), ([13,-3,7,5,9], "13,-3")]
    for test_pair in smoke_tests:
        test_list, response = test_pair
        print(f"\n\nTEST LIST: {test_list}  RESPONSE: {response}")
-       print("SIMPLE VERSION")
-       find_pairs_simple(test_list)
-       print("DICTIONARY VERSION")
-       find_pairs(test_list)
+       print(f"SIMPLE VERSION: {find_pairs_simple(test_list)}")
+       print(f"DICTIONARY VERSION: {find_pairs(test_list)}")
    
 #EOF
