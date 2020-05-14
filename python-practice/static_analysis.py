9bff6d7a26236685a0a55a26ed2c8c1f5c58b735
Dave For Projects <daveforprojects@gmail.com>
2020-05-14 12:40:15 -0400

    Notes on static analysis article

---
+++ python-practice/static_analysis.py
@@ -0,0 +1,10 @@
+import io
+import tokenize
+
+str1 = "This is string one"
+str2 = "This is string two"
+
+str1_upper = str1.upper()
+str2_upper = str2.upper()
+
+print(str1, str2, str1_upper, str2_upper)
