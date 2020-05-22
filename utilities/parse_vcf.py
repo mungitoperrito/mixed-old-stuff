132f996965cd6230be9314ed7b2d6d7949bfcf04
Dave For Projects <daveforprojects@gmail.com>
2020-05-22 19:05:34 -0400

    Improve function name, add skeleton for case statement

--- utilities/parse_vcf.py
+++ utilities/parse_vcf.py
@@ -3,14 +3,60 @@
 VCF_FILES = ['001.vcfmod', '002.vcfmod']
 
 
-def openfile(file):
+def get_file(file):
     with open(file) as f:
         raw_file = f.readlines()
 
     return(raw_file)
     
 
-
+def parse_raw(list_of_lines):
+    records = []
+    new_record = False
+    this_record = []
+    for line in list_of_lines:
+        if line == 'BEGIN:VCARD':
+            new_record == True
+        if line == 'END:VCARD':
+            new_record == False
+        
+        if new_record:
+            if line.startswith() == 'N:':
+                pass
+            elif line.startswith() == 'TEL:':
+                # Might be more than one
+                pass
+            elif line.startswith() == 'EMAIL:':
+                pass
+            elif line.startswith() == 'REV:':
+                pass
+            elif line.startswith() == 'item1':
+                pass
+            elif line.startswith() == 'item2':
+                pass
+            elif line.startswith() == 'ORG:':
+                pass
+            elif line.startswith() == 'NOTE:':
+                pass
+            elif line.startswith() == 'BDAY:':
+                pass
+            elif line.startswith() == 'X-SOCIALPROFILE':
+                pass
+            elif line.startswith() == 'REV:':
+                pass
+            else:
+                pass           
+            
+    return len(list_of_lines)
+    
+    
+def main():
+    output = []
+    
+    for vcf_file in VCF_FILES:
+            output.append(parse_raw(get_file(vcf_file)))
+            
+    print(output)        
 
 if __name__ == "__main__":
     main()
--- utilities/test_parse_vcf.py
+++ utilities/test_parse_vcf.py
@@ -3,7 +3,7 @@ import parse_vcf as pv
 
 
 
-def test_openfile():
+def test_get_file():
     # There is a list of files to parse in the main script
     #   test to make sure a file is opened and returns a list
-    assert str(type(pv.openfile(pv.VCF_FILES[0]))) == "<class 'list'>"
+    assert str(type(pv.get_file(pv.VCF_FILES[0]))) == "<class 'list'>"
