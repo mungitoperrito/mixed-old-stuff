01b5d32b7e45b8e1bf685c17c81cc9fb1340636a
Dave For Projects <daveforprojects@gmail.com>
2020-05-22 20:26:09 -0400

    Add test for parse_raw()

--- utilities/parse_vcf.py
+++ utilities/parse_vcf.py
@@ -14,49 +14,59 @@ def parse_raw(list_of_lines):
     records = []
     new_record = False
     this_record = []
+    unparsed_records = []
+    
     for line in list_of_lines:
-        if line == 'BEGIN:VCARD':
-            new_record == True
-        if line == 'END:VCARD':
-            new_record == False
+        if line.startswith('BEGIN:VCARD', 0):
+            new_record = True
+        if line.startswith('END:VCARD', 0):
+            new_record = False
         
         if new_record:
-            if line.startswith() == 'N:':
+            if line.startswith('N:', 0):
                 pass
-            elif line.startswith() == 'TEL:':
+            elif line.startswith('TEL:', 0):
                 # Might be more than one
                 pass
-            elif line.startswith() == 'EMAIL:':
+            elif line.startswith('EMAIL:', 0):
                 pass
-            elif line.startswith() == 'REV:':
+            elif line.startswith('REV:', 0):
                 pass
-            elif line.startswith() == 'item1':
+            elif line.startswith('item1', 0):
                 pass
-            elif line.startswith() == 'item2':
+            elif line.startswith('item2', 0):
                 pass
-            elif line.startswith() == 'ORG:':
+            elif line.startswith('ORG:', 0):
                 pass
-            elif line.startswith() == 'NOTE:':
+            elif line.startswith('NOTE:', 0):
                 pass
-            elif line.startswith() == 'BDAY:':
+            elif line.startswith('BDAY:', 0):
                 pass
-            elif line.startswith() == 'X-SOCIALPROFILE':
+            elif line.startswith('X-SOCIALPROFILE', 0):
                 pass
-            elif line.startswith() == 'REV:':
-                pass
+            elif line.startswith('BEGIN', 0):
+                # Ignore the new record toggle
+                pass    
             else:
-                pass           
-            
-    return len(list_of_lines)
+                unparsed_records.append(line)           
+    
+    return (records, unparsed_records)
     
     
 def main():
     output = []
+    unparsed_output = []
     
     for vcf_file in VCF_FILES:
-            output.append(parse_raw(get_file(vcf_file)))
-            
-    print(output)        
-
+            records = parse_raw(get_file(vcf_file))
+            output.extend(records[0])
+            unparsed_output.extend(records[1])
+    
+    print(unparsed_output)    
+    return (len(output), len(unparsed_output))
+    
+    
 if __name__ == "__main__":
-    main()
+    (good, bad) = main()
+    print(f"GOOD: {good}  BAD: {bad}")
+
--- utilities/test_parse_vcf.py
+++ utilities/test_parse_vcf.py
@@ -7,3 +7,24 @@ def test_get_file():
     # There is a list of files to parse in the main script
     #   test to make sure a file is opened and returns a list
     assert str(type(pv.get_file(pv.VCF_FILES[0]))) == "<class 'list'>"
+
+
+def test_parse_raw():
+    test_list = ['BEGIN:VCARD', 'junk', 'END:VCARD']
+    (good, bad) = pv.parse_raw(test_list)
+    
+    assert len(good) == 0
+    assert len(bad) == 1
+
+
+def parse_raw(list_of_lines):
+    records = []
+    new_record = False
+    this_record = []
+    unparsed_records = []
+    
+    for line in list_of_lines:
+        if line.startswith('BEGIN:VCARD', 0):
+            new_record = True
+        if line.startswith('END:VCARD', 0):
+            new_record = False
