fb97bb125cb515a7ca05c14dbb2c1ea9bd79b934
Dave For Projects <daveforprojects@gmail.com>
2020-05-31 12:30:55 -0400

    In progress: create_key tests

--- utilities/backup_encryption.py
+++ utilities/backup_encryption.py
@@ -3,14 +3,15 @@ from cryptography.fernet import Fernet
 from datetime import datetime
 
 
-def create_key(now = datetime.now()):
+def create_key():
     key = Fernet.generate_key()
+    now = datetime.now()
     timestamp = now.strftime("%Y-%m-%d-%H%M%S")
     keyfile = "backup_file_encryption.." + timestamp + ".key"
     with open(keyfile, "wb") as key_file:
         key_file.write(key)
-    
-    return keyfile
+
+    return (key, keyfile)
     
         
 def load_key(key="development.key"):
--- utilities/test_backup_encryption.py
+++ utilities/test_backup_encryption.py
@@ -3,13 +3,30 @@ from datetime import datetime
 
 
 def test_create_key():
-    test_date = datetime.datetime(1969, 7, 16, 4, 17, 0)
-    key = 'aa'
-    assert be.create_key(test_date) == False
+    # Key should look like this
+    # key =  b'5jApZKjlxj4SjH5FU05tHx4abfUkzHxgYc3LxFmf4HM='
+    (key, keyfile) = be.create_key()
+    assert len(key) == 44
+    assert len(keyfile) == 45
         
 def test_load_key():
     assert be.load_key(key="development.key") == False
     
 
-def test_encrypt_file(filename, key):
+def test_encrypt_file():
     assert be.encrypt_file(filename, key) == False
+    
+#######################
+'''
+b'PrZBGII55Gs4sVpogQpddmOJ83rJEnVCaCixpdPF3zc='
+b'5jApZKjlxj4SjH5FU05tHx4abfUkzHxgYc3LxFmf4HM='
+b'PiW6myoHpzRkLuLIhe2Vd4sQ6wj7hri0PnFFN1ButCM='
+b'CIFiBtvSJt04DAWgz-e6hRN9boJLnJrA7YkjcVjLQH8='
+b'QKWJH8F4_VuRpY3fwO3NLZRVPU0szpfXN4q6LsJO6nM='
+b'e9L6IPkG1TZOVgqQ5et-OgUGPPWJQA4aDztDea1sTdo='
+b'cP2HOftDBubRBaRxAoCbXJnKR5Khu3bcbmEPptyiqPI='
+b'BVMNNs97mlGdf72iv93RDlw7IDJbup4LryUPE1pIYzE='
+b'nhPOmGjoteRIclQYnW0Az8n1EN-DIhYbmzktFQW-tdc='
+b'NjLVQo0xARjQFM1Lobw1CBpE3KPxnUCHAGeEMoUdvJY='
+b'bHgyYfPDKVZ1j7gjZhoRqaLzwKpra1XB7sGmn0vQ_Ps='
+'''
