4600b2f182063aa076584919c4411683bec20be8
Dave For Projects <daveforprojects@gmail.com>
2020-06-04 20:57:31 -0400

    Refactor load_key, ensure failing test

--- utilities/backup_encryption.py
+++ utilities/backup_encryption.py
@@ -1,9 +1,16 @@
-### A collection of utlities for encrypting files and saving them to gdrive
-from cryptography.fernet import Fernet
-from datetime import datetime
+"""
+    A collection of utlities for encrypting files and saving them to gdrive
+"""
 
+from datetime import datetime
+from cryptography.fernet import Fernet
 
 def create_key():
+    """
+    Generate an encryption key. During development this was run once
+    and the key renamed to 'development.key'. To avoid saving keys to
+    github, .gitignore was updated to ignore *.key files
+    """
     key = Fernet.generate_key()
     now = datetime.now()
     timestamp = now.strftime("%Y-%m-%d-%H%M%S")
@@ -12,15 +19,27 @@ def create_key():
         key_file.write(key)
 
     return (key, keyfile)
-    
-        
+
+
 def load_key(key="development.key"):
-    return open(key, "rb").read()
+    """
+    Read in a previously generated key. Defaults to the dev key
+    """
+    secret_key = 'Not yet loaded'
     
+    try:
+        #loaded = open(key, "rb")
+        loaded = open("THIS_WILL_FAIL", "rb")
+        secret_key = loaded.read()
+    except IOError as ioe:
+        print(f"ERR: IO Error {ioe}")
+    
+    return secret_key
+
 
 def encrypt_file(filename, key):
     encrypter = Fernet(key)
-    
+
     with open(filename, "rb") as file:
         file_data = file.read()
     encrypted_data = encrypter.encrypt(file_data)
@@ -30,25 +49,25 @@ def encrypt_file(filename, key):
 
 def write_encrypted_file(filename, encrypted_data):
     filename = filename + '.enc'
-    
+
     with open(filename, "wb") as file:
         file.write(encrypted_data)
 
     return filename
 
-    
+
 def decrypt_file(filename, key):
     decrypter = Fernet(key)
-    
+
     with open(filename, 'rb') as file:
         encrypted_data = file.read()
     decrypted_data = decrypter.decrypt(encrypted_data)
 
     return str(decrypted_data.decode())
-   
-    
+
+
 if __name__ == "__main__":
-   # Be careful not too lose or overwrite the key as that will render 
-   #    previously encrypted files useless
-   print(f"CREATED KEYFILE: {create_key()}")
+    # Be careful not too lose or overwrite the key as that will render
+    #    previously encrypted files useless
+    print(f"CREATED KEYFILE: {create_key()}")
    
