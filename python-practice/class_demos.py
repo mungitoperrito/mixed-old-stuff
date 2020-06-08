977c2fb053cda440a1ed4a1fb376eab9589ec627
Dave For Projects <daveforprojects@gmail.com>
2020-06-08 16:18:44 -0400

    Examples of class properties

---
+++ python-practice/class_demos.py
@@ -0,0 +1,57 @@
+# Notes on classes
+
+
+class Sample():
+    def __init__(self, name, number):
+        self.name = name
+        self.number = number
+        
+    def print_values(self):
+        print(f"name: {self.name}")
+        print(f"number: {self.number}")
+
+
+class SampleWithProperties():
+    def __init__(self, name, number):
+        self.__name = name
+        self.__number = number
+        
+    @property
+    def name(self):
+        # double underscore is to tell future devs to avoid variable
+        return self.__name
+    
+    @property
+    def double_name(self):
+        # Can return calculated or other values besides fields
+        return 2 * self.__name
+
+
+    @property
+    def number(self):
+        return self.__number
+   
+    @name.setter
+    def name(self, value):
+        self.__name = value
+        
+
+if __name__ == "__main__":
+    '''
+    # Demo Sample()
+    instance = Sample("fred", 3)   
+    instance.print_values()
+    print(f"Access name field directly: {instance.name}")
+    instance.number += 100
+    print(f"Access number field directly: {instance.number}")
+    '''
+    
+    # Demo SampleWithProperties()
+    instance_with_props = SampleWithProperties("fred", 3)   
+    print(f"Access name field, getter: {instance_with_props.name}")
+    print(f"Access name field, getter: {instance_with_props.double_name}")
+    # Next line fails
+    # print(f"Access name field, direct: {instance_with_props.__name}")
+    # Python rewrites value names with intial __ to protect namespace
+    #    not really a private value, but less likely to be accessed
+    print(f"Access name field, direct: {instance_with_props._SampleWithProperties__name}")
