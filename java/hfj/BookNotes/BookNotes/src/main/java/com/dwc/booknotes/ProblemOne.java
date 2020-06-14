e76dc3b23595a7f4cac692918168e23705b885ab
Dave For Projects <daveforprojects@gmail.com>
2020-06-13 23:28:33 -0400

    First Problem - reorder

--- java/hfj/BookNotes/BookNotes/src/main/java/com/dwc/booknotes/ProblemOne.java
+++ java/hfj/BookNotes/BookNotes/src/main/java/com/dwc/booknotes/ProblemOne.java
@@ -11,8 +11,13 @@ package com.dwc.booknotes;
  */
 public class ProblemOne {
     public static void main(String[] args) {
-         int x = 3;
-         while (x > 0){
+        int x = 3;
+        if (x == 3){
+           System.out.print("a");
+        }
+        while (x > 0){
+            x = x - 1;
+            System.out.print("-");
             if (x == 1){
                 System.out.print("d");
                 x = x - 1;
@@ -20,11 +25,6 @@ public class ProblemOne {
             if (x == 2){
                System.out.print("b c");
             } 
-            if (x == 3){
-               System.out.print("a");
-            }
-            x = x - 1;
-            System.out.print("-");
         }
     }
 }
