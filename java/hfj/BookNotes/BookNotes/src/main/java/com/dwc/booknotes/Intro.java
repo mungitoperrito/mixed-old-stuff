17bfcac68f9565325b93521848f9c9fea5f49475
Dave For Projects <daveforprojects@gmail.com>
2020-06-13 22:44:51 -0400

    Basic syntax

--- java/hfj/BookNotes/BookNotes/src/main/java/com/dwc/booknotes/Intro.java
+++ java/hfj/BookNotes/BookNotes/src/main/java/com/dwc/booknotes/Intro.java
@@ -12,5 +12,17 @@ package com.dwc.booknotes;
 public class Intro {
     public static void main(String[] args) {
         System.out.println("Prints to system out");
-    }
+        
+        // This is a comment
+        // This is an integer assignment
+        int x = 1 + 1;
+        
+        System.out.println(x);
+        
+        for (int n = 0; n < 3; n++ ){
+            System.out.print("n: ");
+            System.out.print(n);
+            System.out.print("\n");
+        }
+        
 }
