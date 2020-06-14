304b9227c2e757133a41c3187b02c7bf38329fcb
Dave For Projects <daveforprojects@gmail.com>
2020-06-13 23:42:12 -0400

    Problem C - class method

---
+++ java/hfj/BookNotes/BookNotes/src/main/java/com/dwc/booknotes/ProblemC.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dwc.booknotes;
+
+/**
+ *
+ * @author dave
+ */
+public class ProblemC {
+    // Original missing main method
+    public static void main(String[] arg){
+        int x =5;
+        while (x > 1 ){
+            x = x -1;
+            if ( x < 3 ){
+                System.out.println("Small x");
+            }
+        }
+    }
+}
