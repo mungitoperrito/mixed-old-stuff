793171d2a8de2cef318d2a6bc2f91ff921954441
Dave For Projects <daveforprojects@gmail.com>
2020-06-13 22:04:17 -0400

    Netbeans: check new project creation

---
+++ java/hfj/BookNotes/BookNotes/src/main/java/com/dwc/booknotes/Intro.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dwc.booknotes;
+
+/**
+ *
+ * @author dave
+ */
+public class Intro {
+    public static void main(String[] args) {
+        System.out.println("Prints to system out");
+    }
+}
