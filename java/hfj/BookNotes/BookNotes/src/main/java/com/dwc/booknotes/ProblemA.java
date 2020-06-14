24410fde2cd1baf87912d86ae8d7be12f291bd55
Dave For Projects <daveforprojects@gmail.com>
2020-06-13 23:34:49 -0400

    Problem A - while loop

---
+++ java/hfj/BookNotes/BookNotes/src/main/java/com/dwc/booknotes/ProblemA.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dwc.booknotes;
+
+/**
+ *
+ * @author dave
+ */
+public class ProblemA {
+    public static void main(String[] arg){
+        /* Orig had no decrement in loop, 
+         * only ran below print threshold
+         */
+        int x = 5;
+        while (x < 10){
+            if (x > 3){
+                System.out.println("Big X");
+            }
+        x -= 1    
+        }
+    }
+    
+}
