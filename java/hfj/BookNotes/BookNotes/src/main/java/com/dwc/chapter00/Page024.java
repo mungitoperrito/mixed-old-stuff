4f9edcc207cd0183e16003a5cdb8a2e706129a61
Dave For Projects <daveforprojects@gmail.com>
2020-06-14 14:57:42 -0400

    Exercise

--- java/hfj/BookNotes/BookNotes/src/main/java/com/dwc/chapter00/Page024.java
+++ java/hfj/BookNotes/BookNotes/src/main/java/com/dwc/chapter00/Page024.java
@@ -11,6 +11,25 @@ package com.dwc.chapter00;
  */
 public class Page024 {
     public static void main(String[] arg){
-        
+        int x = 0;
+        while ( x < 4 ){
+        System.out.print("a");
+        if ( x < 1 ){          
+            System.out.print(" ");
+        }
+        System.out.print("n");
+        if ( x > 1 ){
+           System.out.print(" oyster");            
+           x = x + 2;
+        }     
+        if ( x == 1 ){
+           System.out.print("noys");
+           }
+        if ( x < 1 ){
+           System.out.print("oise");
+           }
+        System.out.println("");
+        x = x + 1;
+        }
     }
 }
