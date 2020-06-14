641d264e893039a88abbcfa363033cd064448560
Dave For Projects <daveforprojects@gmail.com>
2020-06-13 21:48:04 -0400

    Try out netbeans

---
+++ java/hfj/hfjava/src/main/java/com/dwc/hfjava/Main.java
@@ -0,0 +1,14 @@
+/*
+ * netbeans HelloWorld
+ */
+package com.dwc.hfjava;
+
+/**
+ *
+ * @author dave
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+}
