e3b80395899c18291672537e53ab10f0341c7476
Dave For Projects <daveforprojects@gmail.com>
2020-06-25 18:26:00 -0400

    Basic timing

---
+++ python-practice/timeit_experiments.py
@@ -0,0 +1,25 @@
+import timeit
+
+def add(num, reps):
+    return_value = 0
+    for i in range(reps):
+        return_value += num
+    
+    return return_value
+
+    
+def multiply(num, reps):
+    return num * reps
+        
+
+if __name__ == "__main__":
+    NUM = 5
+    REPS = 10
+
+    print(f"ADD: {NUM} {REPS}")
+    # Can't pass a function directly. Timeit needs a callable thing so use lambda
+    # number defaults to 3 repetitions, left at default to show syntax
+    print(timeit.repeat(lambda: add(NUM, REPS), number=50))
+    print()
+    print(f"MULTIPLY: {NUM} {REPS}")
+    print(timeit.repeat(lambda: multiply(NUM, REPS), number=50))
