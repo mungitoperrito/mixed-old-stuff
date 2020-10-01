485ed274361adc04d280fbccebd22db174332402
Dave For Projects <daveforprojects@gmail.com>
2020-10-01 14:52:55 -0400

    Loop to create longer work times

--- utilities/do-some-work.js
+++ utilities/do-some-work.js
@@ -1,19 +1,23 @@
 // Create some load/usage in mongodb for testing
 // Load using: mongoshell, load("/path/to/file/do-some-work.js")
 
-db.work.drop()
-
+var LOOP = 5;
 var MAX_COUNT = 10000
-var count = 0;
 
-db.work.createIndex({ "a": 1, "b": 1, "c": 1, "d": 1 })
-db.work.createIndex({ "a": 1, "b": -1, "c": 1, "d": -1 })
-db.work.createIndex({ "a": -1, "b": 1, "c": -1, "d": 1 })
-db.work.createIndex({ "a": -1, "b": -1, "c": -1, "d": -1 })
+for (var i = 0; i < LOOP; i++) {
+  //db.work.drop()
 
-while (count < MAX_COUNT) {
-    db.work.insertOne(
-       {a: count, b: count * count, c: count * count * count, d: 'x'.repeat(count)}
-    )
-    count += 1;
+  var count = 0;
+
+  db.work.createIndex({ "a": 1, "b": 1, "c": 1, "d": 1 })
+  db.work.createIndex({ "a": 1, "b": -1, "c": 1, "d": -1 })
+  db.work.createIndex({ "a": -1, "b": 1, "c": -1, "d": 1 })
+  db.work.createIndex({ "a": -1, "b": -1, "c": -1, "d": -1 })
+
+  while (count < MAX_COUNT) {
+      db.work.insertOne(
+         {a: count, b: count * count, c: count * count * count, d: 'x'.repeat(count)}
+      )
+      count += 1;
+  }
 }
