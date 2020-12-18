311dfe26e8e3103c546af3e6a232ad82a11796d9
Dave For Projects <daveforprojects@gmail.com>
2020-12-18 18:34:49 -0500

    Structure program better

--- utilities/ticker-update.py
+++ utilities/ticker-update.py
@@ -3,27 +3,41 @@ from bs4 import BeautifulSoup
 
 URL = 'https://finance.yahoo.com/quote/'
 CONF_FILE = "ticker-updates.conf"
-secutities = []
 
-with open(CONF_FILE, "r") as conf_file:
-    securities = conf_file.readlines()
-    securities = [s.strip() for s in securities]
-    
-for security in securities:
+
+def get_securities_list():
+   with open(CONF_FILE, "r") as conf_file:
+      securities = conf_file.readlines()
+      securities = [s.strip() for s in securities]
+ 
+   return securities
+
+   
+def update_information(security):
    symbol, sell_price = security.split(',')
-   sell_price = float(sell_price)
-   query = URL + symbol
 
+   query = URL + symbol
    page = requests.get(query)
    soup = BeautifulSoup(page.content, 'html.parser')
 
    span = soup.find('span', {'class': "Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)"})
-   price = float(span.get_text())
-
    table_row = soup.select('table td')
-   open = float(table_row[3].text)
    
-   print(f"{symbol:>6}:  {open:<6}  {price:<6}  "
+   sell_price = float(sell_price)
+   price = float(span.get_text())
+   open_price = float(table_row[3].text)
+   
+   print(f"{symbol:>6}:  {open_price:<6}  {price:<6}  "
          f"{sell_price:<6}  {sell_price - price:<6.3f}  "
          f"{(sell_price - price) / sell_price :<6.2f}"
          )
+
+
+############
+### MAIN ###
+############
+securities = get_securities_list()
+for security in securities:
+    update_information(security)
+    
+# EOF
