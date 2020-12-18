0b2931f011b2e9a51356cdc209914f3ac9b91870
Dave For Projects <daveforprojects@gmail.com>
2020-12-18 17:38:44 -0500

    Remove debug prints, reformat output

--- utilities/ticker-update.py
+++ utilities/ticker-update.py
@@ -8,19 +8,19 @@ secutities = []
 with open(CONF_FILE, "r") as conf_file:
     securities = conf_file.readlines()
     securities = [s.strip() for s in securities]
-    print(securities)
     
 for security in securities:
-   print(security)
    symbol, sell_price = security.split(',')
-   print(f"sy: {symbol}  p: {sell_price}")
+   sell_price = float(sell_price)
    query = URL + symbol
 
    page = requests.get(query)
    soup = BeautifulSoup(page.content, 'html.parser')
+
    span = soup.find('span', {'class': "Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)"})
    price = float(span.get_text())
+
    table_row = soup.select('table td')
    open = float(table_row[3].text)
    
-   print(f"{symbol:>6}: {sell_price:<6}  {open:<6}  {price:<6}  {open - price:<6}")
+   print(f"{symbol:>6}: {open:<6}  {price:<6}  {sell_price:<6}  {sell_price - price:<6.3f}")
