5b7de3cd783c559201929ebfb472e051a4aa7ba3
Dave For Projects <daveforprojects@gmail.com>
2024-12-20 14:14:09 -0500

    Delete java files

--- java/CodeExecutionCheck/src/NeedsAREPL/CheckRandom.java
+++
@@ -1,8 +0,0 @@
-package NeedsAREPL;
-
-public class CheckRandom {
-    public static void main(String[] args){
-        int targetNumber = (int) (Math.random() * 1000);
-            System.out.println("TARGET: " + Integer.toString(targetNumber));
-    }
-}
--- java/headFirstJava/chapter03/Hobbits.java
+++
@@ -1,9 +0,0 @@
-package chapter03;
-
-public class Hobbits {
-	String name;
-	
-	public Hobbits() {
-		this.name = "DEFAULT NAME";		
-	}
-}
