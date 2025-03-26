de38aeffbe445d6b2591aaac5c84fdca566d80b0
Dave For Projects <daveforprojects@gmail.com>
2025-03-26 12:32:17 -0400

    Deletions

--- rename_fotos/.flaskenv
+++
@@ -1,6 +0,0 @@
-#Setup environment variables
-LC_ALL=C.UTF-8
-LANG=C.UTF-8
-FLASK_APP=rename_fotos.py
-FLASK_ENV=development
-
--- rename_fotos/rename_fotos.py
+++
@@ -1,5 +0,0 @@
-# kick off script for foto renaming application
-from app import app
-
-if __name__ == "__main__":
-    app.run(debug=True)
